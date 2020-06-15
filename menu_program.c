bf8ec723ed249c5494688f8d82c5085e561431e7
ilhamfathur27 <ilhammrohang@gmail.com>
2020-06-15 11:07:30 +0700

    menambahkan fungsi pengurangan

--- menu_program.c
+++ menu_program.c
@@ -9,13 +9,21 @@ double tambah(double a, double b) {
 	return a + b;
 }
 
+double kurang(double a, double b) {
+	return a - b;
+
+double kali(double a, double b) {
+	return a - b;
+
 int main() {
 	int pilihan;
 	double angka1, angka2;
 
 	printf("Menu Program\n");
 	printf("----------------------\n");
-	printf("1. Tambah\n");
+	printf("1. pertambahan\n");
+	printf("2. pengurangan\n");
+	printf("3. perkalian\n");
 
 	printf("Masukkan pilihan Anda : ");
 	scanf("%d", &pilihan);
@@ -25,12 +33,21 @@ int main() {
 	printf("Masukkan Angka II : ");
 	scanf("%lf",&angka2);
 
-	switch (pilihan) 
+	switch (pilihan)
 	{
 		case 1:
 			printf("Hasil penjumlahan : ");
 			printf("%.1f\n", tambah(angka1, angka2));
 		break;
+		case 2:
+			printf("Hasil pengurangan : ");
+			printf("%.1f\n", kurang(angka1, angka2));
+		break;
+		case 3:
+			printf("Hasil perkalian : ");
+			printf("%.1f\n", kali(angka1, angka2));
+		break;
+
 	}
 	return 0;
 }
